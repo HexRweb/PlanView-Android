a23f3aa443a151ab0195def2fa8408b36b93b789
Vikas <vikaspotluri123@gmail.com>
2016-08-05 12:55:31 -0500

    Finished notes

--- www/js/main.js
+++ www/js/main.js
@@ -5,7 +5,10 @@ $(document).ready(function(){
 	pv.updateOption = pv.updateOption || function(option,value,callback){localStorage.setItem(option,value); console.log("Dun diz",localStorage.getItem(option));/* Code to update global preferences*/ callback();};
 	pv.getOption = pv.getOption || function(option){return localStorage.getItem(option);};
 	pv.updateLinks = pv.updateLinks || function(){let prefix="#block-";for(let i=1; i<=8;i++){$(prefix+i).attr("data-location",pv.getOption((prefix+i).replace(/#/g,"")));}};
-	pv.updateNotes = pv.updateNotes || function(){let name="block-{{ID}}-notes"};
+	pv.resetNotes = pv.resetNotes || function(){localStorage.setItem("notes",JSON.stringify({1:"",2:"",3:"",4:"",5:"",6:"",7:"",8:""}))};
+	pv.updateNotes = pv.updateNotes || function(){let name="block-{{ID}}-notes";for(let i=1; i<=8;i++){($("#"+name.replace(/{{ID}}/g,i)).val(pv.getNote(i)))}};
+	pv.getNote = pv.getNote || function(note){let json = JSON.parse(localStorage.getItem("notes")); return (json[note]) ? json[note] : ""};
+	pv.saveNote = pv.saveNote || function(which,what){let json = JSON.parse(localStorage.getItem("notes")); json[which] = what; localStorage.setItem("notes",JSON.stringify(json))};
 	$(".class").click(pv.classClick);
 	$('.button-collapse').sideNav();
 	pv.updateLinks();
--- www/js/notes.js
+++ www/js/notes.js
@@ -1,5 +1,12 @@
 $(document).ready(function()
 {
-	$(".btn-save").click(function(){});
-	localStorage.getItem("notes") != null ? pv.updateNotes() :	localSotrage.setItem("notes",JSON.stringify([]));
+	$(".btn-save").click(function()
+	{
+		let block = $(this).attr("data-block");
+		console.log(block,$("#block-"+block+"-notes").val());
+		pv.saveNote(block,$("#block-"+block+"-notes").val());
+		Materialize.toast("Saved",5000);
+	});
+	if(localStorage.getItem("notes") == null) resetNotes(); //Ensures that the notes start out ready.
+	pv.updateNotes();
 })
