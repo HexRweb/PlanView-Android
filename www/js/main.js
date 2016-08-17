4d9f69f75f1d776635baf45033705718c3976095
Vikas <vikaspotluri123@gmail.com>
2016-08-17 00:22:18 -0500

    Lots of changes. See desc
    - Expand multi-line functions (PV-object specific)
    - Comment out logging
    - Add return to pv.saveNote, based on current and toUpdate value
    - Check if note actually saves before toasting

--- www/js/main.js
+++ www/js/main.js
@@ -2,13 +2,35 @@
 window.pv = window.pv || {};
 pv.openClass = pv.openClass || function(link){window.open(link, '_system', 'location=yes')};
 pv.classClick = pv.classClick || function(e){return pv.openClass($(this).attr("data-location"));};
-pv.updateOption = pv.updateOption || function(option,value,callback){localStorage.setItem(option,value); console.log("Dun diz",localStorage.getItem(option));/* Code to update global preferences*/ callback();};
+pv.updateOption = pv.updateOption || function(option,value,callback)
+{
+	localStorage.setItem(option,value);
+	//console.log("Dun diz",localStorage.getItem(option));/* Code to update global preferences*/
+	if(typeof callback === "function") callback();
+};
 pv.getOption = pv.getOption || function(option){return localStorage.getItem(option);};
-pv.updateLinks = pv.updateLinks || function(){let prefix="#block-";for(let i=1; i<=8;i++){$(prefix+i).attr("data-location",pv.getOption((prefix+i).replace(/#/g,"")));}};
+pv.updateLinks = pv.updateLinks || function()
+{
+	let prefix="#block-";
+	for(let i=1; i<=8;i++)
+	{
+		$(prefix+i).attr("data-location",pv.getOption((prefix+i).replace(/#/g,"")));
+	}
+};
 pv.resetNotes = pv.resetNotes || function(){pv.updateOption("notes",JSON.stringify({1:"",2:"",3:"",4:"",5:"",6:"",7:"",8:""}),function(){})};
 pv.updateNotes = pv.updateNotes || function(){let name="block-{{ID}}-notes";for(let i=1; i<=8;i++){($("#"+name.replace(/{{ID}}/g,i)).val(pv.getNote(i)))}};
 pv.getNote = pv.getNote || function(note){let json = JSON.parse(pv.getOption("notes")); return (json[note]) ? json[note] : ""};
-pv.saveNote = pv.saveNote || function(which,what){let json = JSON.parse(pv.getOption("notes")); json[which] = what; pv.updateOption("notes",JSON.stringify(json),function(){})};
+pv.saveNote = pv.saveNote || function(which,what)
+{
+	let json = JSON.parse(pv.getOption("notes"));
+	if(json[which] !== what)
+	{
+		json[which] = what;
+		pv.updateOption("notes",JSON.stringify(json))
+		return true;
+	}
+	else return false;
+};
 $(".class").click(pv.classClick);
 $('.button-collapse').sideNav();
 pv.updateLinks();
@@ -19,22 +41,22 @@ pv.updateLinks();
 $(".btn-save").click(function()
 {
 	let block = $(this).attr("data-block");
-	console.log(block,$("#block-"+block+"-notes").val());
-	pv.saveNote(block,$("#block-"+block+"-notes").val());
-	Materialize.toast("Saved",5000);
+	//console.log(block,$("#block-"+block+"-notes").val());
+	if(pv.saveNote(block,$("#block-"+block+"-notes").val()))
+		Materialize.toast("Saved",5000);
 });
 if(pv.getOption("autosave") == true)
 	$(".materialize-textarea").change(function(){
 		let block = $(this).attr("data-block");
-		console.log(block,$(this).val());
-		pv.saveNote(block,$(this).val());
-		Materialize.toast("Saved",5000);
+		//console.log(block,$(this).val());
+		if(pv.saveNote(block,$(this).val()))
+			Materialize.toast("Saved",5000);
 	});
 	$(".materialize-textarea").focusout(function(){
 		let block = $(this).attr("data-block");
-		console.log(block,$(this).val());
-		pv.saveNote(block,$(this).val());
-		Materialize.toast("Saved",5000);
+		//console.log(block,$(this).val());
+		if(pv.saveNote(block,$(this).val()))
+			Materialize.toast("Saved",5000);
 	});
 if(localStorage.getItem("notes") == null) pv.resetNotes(); //Ensures that the notes start out ready.
 pv.updateNotes();
